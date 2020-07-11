7d358212a6d93cd9625784dba654be6e25b761a7
Jaeho <49357353+jeaho0613@users.noreply.github.com>
2020-07-11 19:54:14 +0900

    LoLAutoClick
    - 롤 오토 클릭
    - c# 과 python 기반
    - 예외처리와 클릭 좌표만 구하면 끝

---
+++ LoLClick/LoLClick/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace LoLClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Click = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MousePosX = new System.Windows.Forms.TextBox();
+            this.MousePosY = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Click
+            // 
+            this.Click.Location = new System.Drawing.Point(12, 12);
+            this.Click.Name = "Click";
+            this.Click.Size = new System.Drawing.Size(75, 23);
+            this.Click.TabIndex = 0;
+            this.Click.Text = "Click";
+            this.Click.UseVisualStyleBackColor = true;
+            this.Click.Click += new System.EventHandler(this.Click_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Y";
+            // 
+            // MousePosX
+            // 
+            this.MousePosX.Location = new System.Drawing.Point(12, 101);
+            this.MousePosX.Name = "MousePosX";
+            this.MousePosX.Size = new System.Drawing.Size(100, 21);
+            this.MousePosX.TabIndex = 3;
+            // 
+            // MousePosY
+            // 
+            this.MousePosY.Location = new System.Drawing.Point(131, 103);
+            this.MousePosY.Name = "MousePosY";
+            this.MousePosY.Size = new System.Drawing.Size(100, 21);
+            this.MousePosY.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 147);
+            this.Controls.Add(this.MousePosY);
+            this.Controls.Add(this.MousePosX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Click);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Click;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MousePosX;
+        private System.Windows.Forms.TextBox MousePosY;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
